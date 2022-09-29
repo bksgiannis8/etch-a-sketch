52df662046480fec7b98b7d171ef17cdcf66ed21
giannis <bkgiannis8@gmail.com>
2022-09-30 01:15:50 +0300

    Make all the divs

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,14 @@
 const container = document.createElement("div");
+container.classList.add("container")
 document.body.appendChild(container);
 
-const square = document.createElement('div');
-square.classList.add(square);
+
+for (let i=0; i<16; i++) {
+    for (let j=0; j<16; j++) {
+        let square = document.createElement('div');
+        square.setAttribute("grid-column", j + " / span 1");
+        square.setAttribute("grid-row", i + " / span 1");
+        square.setAttribute("class", "square");
+        container.appendChild(square);
+    }    
+}
