8d6dc3ca6c5cb3d614bdfb65741aa56a95b731bc
giannis <bkgiannis8@gmail.com>
2022-09-30 18:34:16 +0300

    Update hover over box function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,29 @@
-const container = document.createElement("div");
-container.classList.add("container");
-document.body.appendChild(container);
+const container = document.createElement("div"); // create container element
+container.classList.add("container");   // Add container class
+document.body.appendChild(container);   //append container div to body
+
+const button = document.createElement("button"); //Create button element
+button.classList.add("btn1");                    //Add btn1 class
+document.body.insertBefore(button, container);   //Append button as a child to container
+
+const para = document.createElement("p");
+const node = document.createTextNode("Click me to specify boxes");
+para.appendChild(node);
+button.appendChild(para);
 
 
-for (let i=0; i<16; i++) {
+function createSpace(size) {
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
     for (let j=0; j<16; j++) {
         let square = document.createElement('div');
-        square.setAttribute("grid-column", j + " / span 1");
-        square.setAttribute("grid-row", i + " / span 1");
-        square.setAttribute("class", "square");
         container.appendChild(square);
-    }    
+        square.addEventListener('mouseover', e =>{
+        square.style.backgroundColor = "white";
+        square.classList.add("permahover");
+        })
+    }   
 }
 
 const boxes = document.getElementsByClassName('square');
@@ -21,14 +34,7 @@ for (const box of boxes) {
     });
 };
 
-const button = document.createElement("button");
-button.classList.add("btn1");
-document.body.insertBefore(button, container);
 
-const para = document.createElement("p");
-const node = document.createTextNode("Click me to specify boxes");
-para.appendChild(node);
-button.appendChild(para);
 
 
 
@@ -37,10 +43,10 @@ button.appendChild(para);
 
 
 button.addEventListener("click", () => {
-    let newBoxes=prompt("Please enter number of boxes by row or column");
+    let number=prompt("Please enter number of boxes by row or column");
     document.body.removeChild(container)
     const container=document.createElement("div")
-    document.body.appendChild(container);
+    document.body.appendChild(ontainer);
     container.classList.add("container");
     for (let i=0; i<newBoxes; i++) {
         for (let j=0; j<newBoxes; j++) {
@@ -55,3 +61,5 @@ button.addEventListener("click", () => {
     }
     
 })
+
+
